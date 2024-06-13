82e2de4eead5e3f42ee77497b90583e08827e5c5
Carlos Agamez <carlos.agamez@sigma3ds.com>
2024-06-13 12:38:22 -0500

    hosts by petitions update

--- config/settings.py
+++ config/settings.py
@@ -150,6 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",  
     "https://78css3zt-4200.use.devtunnels.ms",
-    "http://44.203.49.198"
+    "http://44.203.49.198",
+    "http://www.trueshield.one"
     ]
 
